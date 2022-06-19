f35b7f1b41ea59fdc216b2a4ce16cb506573f7f6
scarhead01 <barrogajunjihad1190164@gmail.com>
2022-06-19 17:30:57 +0800

    fdxg

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import {ApolloProvider} from '@apollo/react-hooks';
 import {setContext}  from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri:'http://localhost:5000/graphql'
+    uri:'http://localhost:5000/graphql'||'https://aimsquizzer.herokuapp.com/graphql'
 
 });
 //'https://aimsquizzer.herokuapp.com/graphql'|| 
